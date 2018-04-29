c630f3f030987d14db920f63055541c8585d67f2
Ranol <asdhugh1@gmail.com>
2018-04-29 09:31:01 +0900

    Add lazy logging method

--- common/src/main/kotlin/io/github/kotlinmp/kotlog/Logger.kt
+++ common/src/main/kotlin/io/github/kotlinmp/kotlog/Logger.kt
@@ -23,3 +23,33 @@ expect class Logger {
 
     fun warn(message: String, throwable: Throwable? = null, vararg arguments: Any?)
 }
+
+fun Logger.debug(throwable: Throwable? = null, vararg arguments: Any?, message: () -> Any?) {
+    if (isDebugEnabled) {
+        debug(message().toString(), throwable, arguments)
+    }
+}
+
+fun Logger.error(throwable: Throwable? = null, vararg arguments: Any?, message: () -> Any?) {
+    if (isErrorEnabled) {
+        error(message().toString(), throwable, arguments)
+    }
+}
+
+fun Logger.info(throwable: Throwable? = null, vararg arguments: Any?, message: () -> Any?) {
+    if (isInfoEnabled) {
+        info(message().toString(), throwable, arguments)
+    }
+}
+
+fun Logger.trace(throwable: Throwable? = null, vararg arguments: Any?, message: () -> Any?) {
+    if (isTraceEnabled) {
+        trace(message().toString(), throwable, arguments)
+    }
+}
+
+fun Logger.warn(throwable: Throwable? = null, vararg arguments: Any?, message: () -> Any?) {
+    if (isWarnEnabled) {
+        warn(message().toString(), throwable, arguments)
+    }
+}
