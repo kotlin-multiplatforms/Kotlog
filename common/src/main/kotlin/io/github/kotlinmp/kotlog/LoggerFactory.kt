91ad4a4f222f147d6d97297b4f4d13eb1cfb1614
Ranol <asdhugh1@gmail.com>
2018-05-21 16:48:42 +0900

    Add some kdocs

--- common/src/main/kotlin/io/github/kotlinmp/kotlog/LoggerFactory.kt
+++ common/src/main/kotlin/io/github/kotlinmp/kotlog/LoggerFactory.kt
@@ -1,5 +1,16 @@
 package io.github.kotlinmp.kotlog
 
+/**
+ * The [LoggerFactory] is a utility class producing Loggers for multi platform abstraction.
+ *
+ * Please note that all [LoggerFactory] are object.
+ */
 expect object LoggerFactory {
+    /**
+     * Return a logger named according to the name parameter.
+     *
+     * @param name The name of the logger.
+     * @return logger
+     */
     fun getLogger(name: String): Logger
 }
--- common/src/main/kotlin/io/github/kotlinmp/kotlog/Logging.kt
+++ common/src/main/kotlin/io/github/kotlinmp/kotlog/Logging.kt
@@ -1,5 +1,21 @@
 package io.github.kotlinmp.kotlog
 
+/**
+ * A class with logging capabilities.
+ * usage examples
+ * <code>
+ * class ClassWithLogging {
+ *     companion object : Logging("ClassWithLogging")
+ *
+ *     fun test() {
+ *         logger.info("test ClassWithLogging")
+ *     }
+ * }
+ * </code>
+ */
 open class Logging(name: String) {
+    /**
+     * The member that performs the actual logging.
+     */
     val logger: Logger = LoggerFactory.getLogger(name)
 }
