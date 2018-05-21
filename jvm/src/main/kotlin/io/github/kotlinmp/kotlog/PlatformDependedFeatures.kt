eb1c999ffa8a11a8e2b827e51051d7bd9002d91d
Ranol <asdhugh1@gmail.com>
2018-05-21 21:03:43 +0900

    Move class

---
+++ jvm/src/main/kotlin/io/github/kotlinmp/kotlog/PlatformDependedFeatures.kt
@@ -0,0 +1,17 @@
+package io.github.kotlinmp.kotlog
+
+import java.time.LocalDateTime
+
+internal actual object PlatformDependedFeatures {
+    actual fun getCurrentScope(): Scope? {
+        return Scope(Thread.currentThread().name)
+    }
+
+    actual fun currentDateTime(): Pair<Date, Time> {
+        val now = LocalDateTime.now()
+
+        return Pair(Date(now.year, now.monthValue, now.dayOfMonth), Time(now.hour, now.minute, now.second))
+    }
+
+    actual fun getErrorName(throwable: Throwable): String = throwable::class.java.simpleName ?: "Error"
+}
